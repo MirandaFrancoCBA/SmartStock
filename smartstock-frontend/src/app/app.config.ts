8fac7e19a279f1cdc45b4c38a4cb8355e31c58fe
MirandaFrancoCBA <mirandafranco.ispc@gmail.com>
2026-03-22 16:08:56 -0300

    Feat: agregando interceptor jwt

--- smartstock-frontend/src/app/app.config.ts
+++ smartstock-frontend/src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http'; // Importante
+import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
+import { provideZoneChangeDetection } from '@angular/core';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([jwtInterceptor])),
+      provideZoneChangeDetection({ eventCoalescing: true }),
   ]
 };
